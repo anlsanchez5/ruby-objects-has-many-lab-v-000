8feb4a7bd0ec460105d12b5bf9d0aeb051b7f24b
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-05 06:54:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,21 @@
 
 
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
   @@songs = []
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def add_song(song)
     @@songs << song
-    @songs << song
     song.artist = self
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @@songs << song
-    @songs << song
     song.artist = self
   end
 
