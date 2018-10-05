ff6ec49368503039cf1564a115e77f8f4f816026
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-05 06:58:27 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+
+
+class Author
+  attr_accessor :name, :posts
+  @@posts = []
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @@post << post
+    @post << post
+    post.author = self
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @@post << post
+    @post << post
+    post.author = self
+  end
